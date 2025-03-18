643d36363c2a87b70a2ff2028e7f2e2df91a0c9a
Antonio <antonioazevedocabreiral@gmail.com>
2025-03-17 21:19:41 -0300

    selectionSort

--- selectionSort.mjs
+++ selectionSort.mjs
@@ -1,3 +1,4 @@
 "use strict"
 
 // Selection Sort
+// Basically a better bubble sort, it isolates the first in the array/list and then it checks all other values, until it finds the lowest value, this "lowest value" then becomes the first value
