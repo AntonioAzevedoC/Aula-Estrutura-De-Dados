1e535c3a4b4ed103e84c39c0cb894d5d3be6d3ba
AntonioAzevedoC <antonioazevedocabreira@gmail.com>
2025-05-05 21:57:17 -0300

    JS Classes 3 - honestly, only doing this for the commit history

--- OBJECTS/_obj-shapes.mjs
+++ OBJECTS/_obj-shapes.mjs
@@ -2,7 +2,9 @@
 import Shape from "../CLASSES/_js-Classes.mjs";
 
 const sh1 = new Shape(10, 20, "T");
+const sh2 = new Shape();
 
+// Calculating area for sh1
 sh1.calcArea();
 console.log(sh1.getArea);
 
@@ -13,3 +15,10 @@ console.log(sh1.getArea);
 sh1.setType = "E";
 sh1.calcArea();
 console.log(sh1.getArea);
+
+// Defining sh2
+sh2.setHeight = 23;
+sh2.setWidth = 12;
+sh2.setType = "R";
+sh2.calcArea();
+console.log(sh2.getArea);
